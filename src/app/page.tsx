3aad290d48d27c31ebd13d81e1206378878fd76b
Dev-Luigy <luigygabriel18a@gmail.com>
2024-02-22 14:46:06 -0300

    feat: Getting base select from shadcn/ui lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import React from 'react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <main className="h-full w-full flex items-center justify-center">
+      <div className="h-52 w-52 bg-orange-200 flex items-center justify-center">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </main>
+  )
 }
