cc8b2c6cde213bca423aa27813f6ae735cb72313
Daniel Vitor <ripsea.rpg321@gmail.com>
2024-02-24 16:24:09 -0300

    chore: refine Input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,39 +1,72 @@
 'use client'
 
-import { InputHTMLAttributes, memo } from 'react'
+import { InputHTMLAttributes, forwardRef } from 'react'
+import { cva } from 'class-variance-authority'
+import { twMerge } from 'tailwind-merge'
+import { clsx } from 'clsx'
+
 import './input.styles.css'
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name?: string
-  inheritColor?: string
+  name?: string | undefined
+  error?: boolean
+  sz?: 'small' | 'medium' | 'large'
 }
 
-function Input({
-  name,
-  className,
-  inheritColor,
-  width,
-  height,
-  ...props
-}: InputProps) {
-  return (
-    <div className={`relative ${className}`} style={{ width, height }}>
-      <input
-        required
-        id={name}
-        style={{ width, height }}
-        className={`text-xl text-primaryGray bg-transparent border-[0.094rem] rounded-lg border-primaryGray outline-none p-4 focus:border-handle-blue focus:caret-handle-blue transition duration-200 placeholder-transparent peer`}
-        {...props}
-      />
-      <label
-        htmlFor={props?.id}
-        className={`absolute transition-all duration-200 text-sm left-4 px-1 tracking-wider -top-2.5 text-primaryGray ${'bg-'.concat(inheritColor ?? 'background')} peer-focus:text-handle-blue peer-focus:text-sm peer-focus:-top-2.5 peer-focus:translate-y-0 peer-placeholder-shown:text-xl peer-placeholder-shown:text-primaryGray peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2
-        `}
-      >
-        {props?.placeholder}
-      </label>
-    </div>
-  )
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ name, className, placeholder, error, sz = 'medium', ...props }, ref) => {
+    console.log(error)
 
-export default memo(Input) as unknown as typeof Input
+    const variants = cva(
+      clsx({
+        'w-full rounded-md bg-transparent box-border outline-none transition-all duration-300 ease-in-out focus-visible:text-handle-blue border-[0.094rem] border-custom-gray-300 focus-visible:border-handle-blue peer':
+          true,
+        'text-handle-red-500 border-handle-red-500 focus-visible:text-handle-red-500 focus-visible:border-handle-red-500':
+          error,
+      }),
+      {
+        variants: {
+          sz: {
+            small: ['px-2 py-2', 'text-sm'],
+            medium: ['px-3 py-2', 'text-base'],
+            large: ['px-4 py-2', 'text-lg'],
+          },
+        },
+        defaultVariants: {
+          sz: 'medium',
+        },
+      },
+    )
+
+    return (
+      <div className={twMerge('relative bg-inherit', className)}>
+        <input
+          ref={ref}
+          id={name}
+          name={name}
+          type="text"
+          className={twMerge(variants({ sz }))}
+          {...props}
+          placeholder=" "
+        />
+        <label
+          htmlFor={props.id}
+          className={twMerge(
+            'absolute top-0 -translate-y-1/2 left-2 px-1 py-0 text-xs bg-inherit transition-all duration-300 ease-in-out pointer-events-none',
+            clsx({
+              'text-handle-red-500': error,
+              'text-custom-gray-300 peer-focus-visible:text-handle-blue':
+                !error,
+            }),
+          )}
+        >
+          {placeholder}
+        </label>
+      </div>
+    )
+  },
+)
+
+Input.displayName = 'Input'
+
+export { Input }
--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,4 +1,4 @@
-import Input from './Input'
+import { Input } from './Input'
 
 export type { InputProps } from './Input'
 
