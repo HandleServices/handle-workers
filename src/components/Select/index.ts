6291bcf3174a5261ee00f0046926379f8c85e7dd
Gabriel Al-Samir Guimaraes Sales <alsamir.gabriel@gmail.com>
2024-03-10 17:19:09 -0300

    chore: create index file to export select component

---
+++ src/components/Select/index.ts
@@ -0,0 +1,24 @@
+import {
+  Select, 
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton } from "./Select";
+
+export default {
+  Select, 
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton
+}
