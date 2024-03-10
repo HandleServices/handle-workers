39fc24bbfa40aac9f7796af251b10f95a7e4d39d
Gabriel Al-Samir Guimaraes Sales <alsamir.gabriel@gmail.com>
2024-03-10 16:44:07 -0300

    fix: solve some style problems according to figma design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/Select'
+import React from 'react'
 
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <Select>
-        <SelectTrigger className="w-[280px] h-12">
-          <SelectValue placeholder="Selecione sua profissão" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="Cabeleleiro">Cabeleleiro</SelectItem>
-          <SelectItem value="Eletricista">Eletricista</SelectItem>
-          <SelectItem value="Montador">Montador</SelectItem>
-        </SelectContent>
-      </Select>
-    </div>
-  )
+  return <div></div>
 }
