ff31f302de54fb8ca7421bb0f42fbf0597ab2060
Dev-Luigy <luigygabriel18a@gmail.com>
2024-03-16 15:22:42 -0300

    fix: Solving some styling problems and adding rules to EsLint empty interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-'use client'
-import React, { useState } from 'react'
-import { DayOfWeekPicker } from '@/components/DaysOfWeekPicker/DayOfWeekPicker'
+import React from 'react'
 
 export default function Home() {
-  const [value, setValue] = useState([])
-  return (
-    <div>
-      <DayOfWeekPicker
-        value={value}
-        onValueChange={(value) => {
-          if (value) setValue(value)
-        }}
-        className="w-[375px] gap-4"
-      />
-    </div>
-  )
+  return <div></div>
 }
