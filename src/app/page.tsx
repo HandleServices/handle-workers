ceb0033cd8d2f8be6e9d619d28e7c144b2ec391c
Dev-Luigy <luigygabriel18a@gmail.com>
2024-04-02 17:58:11 -0300

    refactor: Returning default value to page.tsx file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
 import React from 'react'
-import { InputImage } from '@/components/InputImage'
 
 export default function Home() {
-  return (
-    <div className='flex flex-row gap-5'>
-      <InputImage className="" width={190} height={155} />
-      <InputImage className="" width={190} height={500} />
-      <InputImage className="" width={190} height={800} />
-      <InputImage className="" width={100} height={150} />
-      <InputImage className="" width={200} height={150} />
-      <InputImage className="" width={150} height={150} />
-      <InputImage className="" width={150} height={100} />
-      <InputImage className="" width={150} height={200} />
-
-    </div>
-
-  )
+  return <div></div>
 }
