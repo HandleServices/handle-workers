dc6265e42e6b9e8be8e86719ff74a2cf0f6090cb
Dev-Luigy <luigygabriel18a@gmail.com>
2024-04-04 17:47:37 -0300

    feat: Creating basic TimePicker component code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import TimePicker from '../components/TimePicker'
+
 export default function Home() {
-  return <div></div>
+  return (
+    <div>
+      <TimePicker className="p-5 w-[200px]" />
+    </div>
+  )
 }
---
+++ src/components/TimePicker/TimePicker.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { parse } from 'path'
+import { useState } from 'react'
+
+import { Input } from '../Input/Input'
+
+interface TimePickerProps {
+  width?: number
+  height?: number
+}
+
+function hourMinuteMask(value: string): string {
+  const numbers = value.replace(/([0-1]?[0-9]|2[0-3]):[0-5][0-9]/g, '')
+  if (numbers.length <= 2) {
+    if (parseInt(numbers) > 23) return '23'
+    return numbers
+  }
+  return `${numbers.slice(0, 2)}:${numbers.slice(2, 4)}`
+}
+
+const TimePicker = ({
+  width = 140,
+  height = 60,
+  ...props
+}: TimePickerProps) => {
+  const [inHour, setInOur] = useState('')
+  const [outHour, setOutOur] = useState('')
+
+  function handleHourMinuteMask(
+    event: { target: { value: string } },
+    setTime: (value: string) => void,
+  ) {
+    setTime(hourMinuteMask(event.target.value))
+  }
+
+  return (
+    <div className="flex flex-row gap-[95px] tracking-widest">
+      <div>
+        <label className="">Início</label>
+        <Input
+          className="text-center "
+          onChange={(event) => handleHourMinuteMask(event, setInOur)}
+          placeholder=""
+          width={width}
+          height={height}
+          value={inHour}
+        />
+      </div>
+      <div>
+        <label>Fim</label>
+        <Input
+          className="text-center"
+          onChange={(event) => handleHourMinuteMask(event, setOutOur)}
+          placeholder=""
+          width={width}
+          height={height}
+          value={outHour}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default TimePicker
