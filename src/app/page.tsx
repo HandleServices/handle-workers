ab874b636b404debeca1d9c247b03f5cf05a022e
Dev-Luigy <luigygabriel18a@gmail.com>
2024-04-09 07:19:47 -0300

    style: TimePicker component style change.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import TimePicker from '../components/TimePicker'
 export default function Home() {
   return (
     <div>
-      <TimePicker className="p-5 w-[200px]" />
+      <TimePicker />
+      <TimePicker width={200} height={100} />
+      <TimePicker width={250} height={150} />
     </div>
   )
 }
--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -10,12 +10,12 @@ interface TimePickerProps {
 }
 
 function hourMinuteMask(value: string): string {
-  const numbers = value.replace(/([0-1]?[0-9]|2[0-3]):[0-5][0-9]/g, '')
+  const numbers = value.replace(/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/g, '')
   if (numbers.length <= 2) {
     if (parseInt(numbers) > 23) return '23'
     return numbers
   }
-  return `${numbers.slice(0, 2)}:${numbers.slice(2, 4)}`
+  return `${numbers.slice(0, 2)}  ${parseInt(numbers.slice(2, 4)) > 59 ? '59' : numbers.slice(2, 4)}`
 }
 
 const TimePicker = ({
@@ -25,36 +25,71 @@ const TimePicker = ({
 }: TimePickerProps) => {
   const [inHour, setInOur] = useState('')
   const [outHour, setOutOur] = useState('')
+  const [onFocus, setOnFocus] = useState([false, false])
 
   function handleHourMinuteMask(
     event: { target: { value: string } },
     setTime: (value: string) => void,
   ) {
     setTime(hourMinuteMask(event.target.value))
+    console.log(inHour)
   }
 
   return (
     <div className="flex flex-row gap-[95px] tracking-widest">
       <div>
         <label className="">Início</label>
+        <div
+          className="absolute"
+          style={{
+            transform: `translate(${width / 2 - (height / 20) * 2 - 3 * (height / 60)}px, ${height / 2 - height / 2 - height / 10}px)`,
+            fontSize: `${height / 20}rem`,
+            visibility: !onFocus[0] ? 'visible' : 'hidden',
+            color: !onFocus[0] ? 'black' : 'transparent',
+          }}
+        >
+          :
+        </div>
         <Input
-          className="text-center "
+          style={{
+            width,
+            height,
+            textAlign: 'center',
+            letterSpacing: '0.1rem',
+            fontSize: `${height / 30}rem`,
+          }}
           onChange={(event) => handleHourMinuteMask(event, setInOur)}
           placeholder=""
           width={width}
           height={height}
           value={inHour}
+          onFocus={() => setOnFocus([true, false])}
+          onBlur={() => setOnFocus([false, false])}
         />
       </div>
       <div>
-        <label>Fim</label>
+        <label className="">Fim</label>
+        <div
+          className="absolute"
+          style={{
+            transform: `translate(${width / 2 - (height / 20) * 2}px, ${height / 2 - height / 2 - height / 10}px)`,
+            fontSize: `${height / 20}rem`,
+          }}
+        >
+          :
+        </div>
         <Input
-          className="text-center"
+          style={{
+            width,
+            height,
+            textAlign: 'center',
+          }}
           onChange={(event) => handleHourMinuteMask(event, setOutOur)}
           placeholder=""
           width={width}
           height={height}
           value={outHour}
+          onFocus={() => setOnFocus([false, true])}
         />
       </div>
     </div>
