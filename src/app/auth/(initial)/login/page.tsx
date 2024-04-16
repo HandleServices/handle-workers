6a33a11bb29d25f8a6c070aba1b7b8a0c0f019d2
Daniel Vitor <ripsea.rpg321@gmail.com>
2024-04-16 01:00:31 -0300

    feature: add zod and react-hook-form to login page

--- src/app/auth/(initial)/login/page.tsx
+++ src/app/auth/(initial)/login/page.tsx
@@ -1,31 +1,71 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
 import * as Separator from '@radix-ui/react-separator'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/Button'
 import Input from '@/components/Input'
+import { LabelError } from '@/components/LabelError'
 
 import SvgComponent from '../assets/google'
 
+const loginSchema = z.object({
+  email: z.string().email({ message: 'Endereço de e-mail inválido.' }),
+  password: z.string().min(8, { message: 'Deve ter no mínimo 8 caracteres.' }),
+})
+
+type LoginType = z.infer<typeof loginSchema>
+
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginType>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  const onSubmit: SubmitHandler<LoginType> = (data) => {
+    console.log('FINISHED')
+    console.log(data)
+    alert(JSON.stringify(data))
+  }
+
   return (
-    <form className="w-full max-w-screen min-h-screen px-5 lg:px-20 py-0 lg:py-10 flex items-center justify-center">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full max-w-screen min-h-screen px-5 lg:px-20 py-0 lg:py-10 flex items-center justify-center"
+    >
       <div className="w-2/3 gap-10 flex flex-col items-center justify-center bg-handle-background">
         <div className="w-full flex flex-col gap-6 bg-handle-background">
-          <Input
-            className="w-full"
-            placeholder="E-mail"
-            customBgColor="bg-handle-background"
-            height="3rem"
-          />
+          <div className="w-full flex flex-col gap-1">
+            <Input
+              {...register('email')}
+              error={!!errors.email}
+              className="w-full"
+              placeholder="E-mail"
+              customBgColor="bg-handle-background"
+              height="3rem"
+            />
 
-          <Input
-            className="w-full"
-            placeholder="Senha"
-            type="password"
-            customBgColor="bg-handle-background"
-            height="3rem"
-          />
+            <LabelError errors={errors} name="email" />
+          </div>
+
+          <div className="w-full flex flex-col gap-1">
+            <Input
+              {...register('password')}
+              error={!!errors.password}
+              className="w-full"
+              placeholder="Senha"
+              type="password"
+              customBgColor="bg-handle-background"
+              height="3rem"
+            />
+
+            <LabelError errors={errors} name="password" />
+          </div>
         </div>
 
         <div className="flex flex-col gap-2">
