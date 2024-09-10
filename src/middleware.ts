cc4c00fe737f4937e6e1fe2cd91d95985a06c4cd
Gabriel Al-Samir Guimaraes Sales <alsamir.gabriel@gmail.com>
2024-09-10 13:18:44 -0300

    fix: removes coments from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,19 @@ export async function middleware(req: NextRequest) {
     'handleworkers.isFirstRegisterComplete',
   )?.value
 
-  // if (req.url.includes('/login') && token) {
-  //   return NextResponse.redirect(new URL('/admin/home', req.url))
-  // }
+  if (req.url.includes('/login') && token) {
+    return NextResponse.redirect(new URL('/admin/home', req.url))
+  }
 
-  // if (req.nextUrl.pathname.startsWith('/admin') && !token) {
-  //   return NextResponse.redirect(new URL('/auth/login', req.url))
-  // }
+  if (req.nextUrl.pathname.startsWith('/admin') && !token) {
+    return NextResponse.redirect(new URL('/auth/login', req.url))
+  }
 
-  // if (req.nextUrl.pathname.startsWith('/auth/complete_register')) {
-  //   if (!isFirstRegisterComplete || isFirstRegisterComplete !== 'true') {
-  //     return NextResponse.redirect(new URL('/auth/register', req.url))
-  //   }
-  // }
+  if (req.nextUrl.pathname.startsWith('/auth/complete_register')) {
+    if (!isFirstRegisterComplete || isFirstRegisterComplete !== 'true') {
+      return NextResponse.redirect(new URL('/auth/register', req.url))
+    }
+  }
 
   return NextResponse.next()
 }
