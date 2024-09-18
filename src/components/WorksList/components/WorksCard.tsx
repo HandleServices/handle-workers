0d9786eaf42de4d7994493e673b56538dba52f1f
Gabriel Al-Samir Guimaraes Sales <alsamir.gabriel@gmail.com>
2024-09-18 18:29:58 -0300

    feat: add animation to 3 dots button on service

--- src/components/WorksList/components/WorksCard.tsx
+++ src/components/WorksList/components/WorksCard.tsx
@@ -1,7 +1,4 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { useEffect, useRef, useState } from 'react'
 
 import DialogButton from '@/components/Dialog/components/DialogButton'
 import { DialogTitle } from '@/components/Dialog/Dialog'
@@ -23,65 +20,88 @@ const WorkCard = ({ className, cardInfo, ...props }: WorkCardProps) => {
   const [editDialogIsOpen, setEditDialogIsOpen] = useState(false)
   const [editDialogIsPending, setEditDialogIsPending] = useState(false)
   const [showOptions, setShowOptions] = useState<boolean>(false)
+  const optionsRef = useRef<HTMLDivElement>(null)
 
-  const serviceSchema = z.object({
-    value: z.number(),
-    name: z.string(),
-    description: z.string(),
-  })
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        optionsRef.current &&
+        !optionsRef.current.contains(event.target as Node) &&
+        !editDialogIsOpen
+      ) {
+        setShowOptions(false)
+      }
+    }
 
-  const {
-    control,
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(serviceSchema),
-  })
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [editDialogIsOpen, optionsRef, showOptions])
 
   return (
-    <div className="grid grid-cols-[1fr_0.12fr_0.10fr] ml-6 min-h-20">
+    <div className="grid grid-cols-[1fr_0.12fr_0.10fr] ml-6 min-h-20 relative">
       <div className="flex flex-col">
         <h1 className="font-semibold tracking-wide">{cardInfo.name}</h1>
         <p className="text-handle-gray text-sm">{cardInfo.description}</p>
       </div>
       <div className="flex flex-row justify-between items-center text-handle-blue text-2xl">
-        <text className="mr-2">R$</text>
-        <text>{cardInfo.value.toFixed(2).replace('.', ',')}</text>
+        <span className="mr-2">R$</span>
+        <span>{cardInfo.value.toFixed(2).replace('.', ',')}</span>
       </div>
-      <div className="flex flex-col justify-center items-center transition-all duration-1000">
+
+      <div className="relative flex flex-col justify-center items-center">
         <button
-          onClick={() => setShowOptions(true)}
-          className={`text-2xl p-0 mb-12 ${showOptions ? 'hidden h-0 p-0 w-0 overflow-hidden' : ''}`}
+          onClick={() => setShowOptions(!showOptions)}
+          className={`duration-500 ease-in-out delay-[1000ms] text-2xl p-0 mb-12 ${
+            showOptions ? 'opacity-0 h-0 w-0 overflow-hidden' : 'opacity-100'
+          }`}
         >
           ...
         </button>
+
         <div
-          className={`flex flex-col transition-all duration-1000 justify-center items-center gap-2 ${showOptions ? 'h-full w-full ' : 'hidden'}`}
+          ref={optionsRef}
+          className={`absolute top-0 flex flex-col justify-center items-center gap-2 transition-all duration-500 ease-in-out overflow-hidden ${
+            showOptions ? 'max-h-32' : 'max-h-0'
+          }`}
+          style={{ top: '50%', transform: 'translateY(-50%)' }} // Centraliza os ícones
         >
-          <DialogButton
-            buttonTitle={
-              <PencilIcon viewBox={`-3 -1 18 18`} height={25} width={25} />
-            }
-            setIsOpen={setEditDialogIsOpen}
-            isSendingData={editDialogIsPending}
-            isOpen={editDialogIsOpen}
-            className="text-3xl flex justify-center items-center"
+          <div
+            className={`transition-opacity duration-500 ease-in-out delay-[100ms] ${
+              showOptions ? 'opacity-100' : 'opacity-0'
+            }`}
           >
-            <DialogTitle>
-              <p className="text-handle-blue text-2xl font-medium select-none mb-0">
-                Editando um serviço...
-              </p>
-              <p className="text-handle-blue text-base font-light select-none mt-0">
-                por favor modifique o que desejar
-              </p>
-            </DialogTitle>
-            <form></form>
-          </DialogButton>
-          <button className="text-3xl flex justify-center items-center">
-            <TrashIcon viewBox={`-5 0 25 25`} height={25} width={25} />
-          </button>
+            <DialogButton
+              buttonTitle={
+                <PencilIcon viewBox={`-3 -1 18 18`} height={25} width={25} />
+              }
+              setIsOpen={setEditDialogIsOpen}
+              isSendingData={editDialogIsPending}
+              isOpen={editDialogIsOpen}
+              className="text-3xl flex justify-center items-center"
+            >
+              <DialogTitle>
+                <p className="text-handle-blue text-2xl font-medium select-none mb-0">
+                  Editando um serviço...
+                </p>
+                <p className="text-handle-blue text-base font-light select-none mt-0">
+                  Por favor, modifique o que desejar.
+                </p>
+              </DialogTitle>
+              <form></form>
+            </DialogButton>
+          </div>
+          <div
+            className={`transition-opacity duration-500 ease-in-out delay-[300ms] ${
+              showOptions ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            <button className="text-3xl flex justify-center items-center">
+              <TrashIcon viewBox={`-5 0 25 25`} height={25} width={25} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
