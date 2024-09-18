a5d85f923923ba3e45ffe88bd1fe341ab393b900
Gabriel Al-Samir Guimaraes Sales <alsamir.gabriel@gmail.com>
2024-09-18 20:08:01 -0300

    fix: fix on worklist component

--- src/components/WorksList/WorksList.tsx
+++ src/components/WorksList/WorksList.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from 'tailwind-merge'
+
 import type { WorkService } from '@/types/models/Service.model'
 
 import WorkCard from './components/WorksCard'
@@ -8,29 +10,25 @@ export interface ServiceListProps {
   className?: string
 }
 
-const WorksList = ({
-  className,
-  worksList,
-  minHeight,
-  ...props
-}: ServiceListProps) => {
+const WorksList = ({ className, worksList, minHeight }: ServiceListProps) => {
   return (
     <div
-      className={`flex flex-col p-2 pb-6 rounded-md w-5/6 py-2 h-full ${className}`}
+      className={twMerge(
+        'flex flex-col p-2 pb-6 rounded-md w-5/6 py-2',
+        className,
+      )}
     >
       <div
         style={{ minHeight: `${minHeight}px` }}
-        className="bg-white h-full px-6 py-2"
+        className="bg-white px-6 py-2 w-full"
       >
         {worksList &&
-          worksList.map((work, index) => {
-            return (
-              <div className="w-full" key={index}>
-                <WorkCard cardInfo={work} />
-                <hr />
-              </div>
-            )
-          })}
+          worksList.map((work, index) => (
+            <div className="w-full" key={index}>
+              <WorkCard cardInfo={work} />
+              <hr />
+            </div>
+          ))}
       </div>
     </div>
   )
